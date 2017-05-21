802065451308418cc8a7154f3a1ad3f87a14cd5d
Phạm Quốc Toàn <bossdiemmaimai@gmail.com>
2017-05-21 01:41:55 -0700

    update

--- policy_DAC1_1412558.sql
+++
@@ -1,9 +0,0 @@
--- Chỉ trưởng phòng được phép cập nhật và thêm thông tin vào dự án (DAC).
-grant insert, update on DUAN to R_TRUONGPHONG;
-
-begin
-for TRUONGPB_LIST in (select TRUONGPHONG from HCMUS.PHONGBAN)
-loop
-  execute immediate 'grant R_TRUONGPHONG to ' || TRUONGPB_LIST.TRUONGPHONG; 
-end loop;
-end;
